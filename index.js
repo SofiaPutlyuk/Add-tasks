bb9e955a2d01b4d38df4bbfed63e949808e7b275
Sofia <putlyuksophia@gmail.com>
2024-07-18 17:17:06 +0300

    hw

---
+++ index.js
@@ -0,0 +1,10 @@
+const searchImg = document.getElementById('photo-dinasour')
+
+document.addEventListener('keydown',function(event){
+let cording = 0 ;
+if(event.key === "Space"){
+    cording += 10
+}
+   
+
+})
